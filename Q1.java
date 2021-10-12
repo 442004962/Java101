c7a933de225e3659f11dcd77d710e0eaabb45bfa
442004962 <91424756+442004962@users.noreply.github.com>
2021-10-12 11:14:44 +0300

    Create Q1.java

---
+++ Q1.java
@@ -0,0 +1,4 @@
+    Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+    System.out.println("Enter username");
+String userName = myObj.nextLine();  // Read user input
+    System.out.println("Username is: " + userName);  // Output user inpu
