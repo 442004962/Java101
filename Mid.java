f497476eb9b59299468b847b5b9b0a4fbc4948a5
442004962 <91424756+442004962@users.noreply.github.com>
2021-10-19 11:40:41 +0300

    Create Mid.java

---
+++ Mid.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner scan = new Scanner (System.in);
+	    int age = 1423;
+	    System.out.println(age);
+	    String name = "abdulmajeed";
+	    System.out.println(name);
+    int year =2021-20;
+  }
+}
+    
+	    
