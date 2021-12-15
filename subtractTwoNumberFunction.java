fc044605b3a06e0fd2a59d98e7748b2b6fb2017a
442004962 <91424756+442004962@users.noreply.github.com>
2021-12-15 17:42:43 +0300

    Create subtractTwoNumberFunction.java

---
+++ subtractTwoNumberFunction.java
@@ -0,0 +1,12 @@
+public class Main {
+    static int add(int x , int y) {
+        return x - y;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(add(8,5));
+
+        
+    }
+}
