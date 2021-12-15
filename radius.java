6b8401c92776f5af4921998f8c8bc875cb817c27
442004962 <91424756+442004962@users.noreply.github.com>
2021-12-15 16:50:08 +0300

    Create radius.java

---
+++ radius.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main {
+public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int num1;
+        System.out.print("Enter the radius");
+
+        num1 = scan.nextInt();
+
+        System.out.print(3.14*num1*num1);
+        }
+
+        }
