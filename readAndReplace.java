c2b68f883fff0a86ed01d9c2c2591eb67261f631
442004962 <91424756+442004962@users.noreply.github.com>
2021-12-15 14:46:42 +0300

    Create readAndReplace.java

---
+++ readAndReplace.java
@@ -0,0 +1,11 @@
+public class Main{
+public static void main(String[] args) {
+     String name = "abdal majeed ";
+     for (int x =0;  x<=8 ; ++x){
+         if(name.charAt(x) == 'a'){
+        System.out.println('x');}
+        else {
+            System.out.println(name.charAt(x));
+        }
+        
+     }}}
