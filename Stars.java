5b246f1ae0bdfc6c5ce6650350aa10faa60e8599
442004962 <91424756+442004962@users.noreply.github.com>
2021-12-15 06:41:19 +0300

    Create Stars.java

---
+++ Stars.java
@@ -0,0 +1,16 @@
+public class Ex {
+public static void main(String[] args) {
+        int a = 0;
+        while(a <= 5){
+        int n = 1;
+        while(n <= a){
+        System.out.print("*");
+        ++n;
+        }
+
+        System.out.println("*");
+
+        ++a;
+        }
+        }
+        }
