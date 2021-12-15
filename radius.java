93269c38187c4ff8764415910408745ad622bc22
442004962 <91424756+442004962@users.noreply.github.com>
2021-12-15 22:13:44 +0300

    Update radius.java

--- radius.java
+++ radius.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Main {
+public class radius.java {
 public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int num1;
