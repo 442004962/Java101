b01676702c51bb92abeb86e645245def745d562f
442004962 <91424756+442004962@users.noreply.github.com>
2021-12-21 10:54:09 +0300

    Create Exam.java

---
+++ Exam.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+
+	    int counter;
+	    for(counter =100; counter>=-100; --counter){
+	        ;
+	        if(counter%2==0){
+	            System.out.println(counter+"odd");
+	       
+	        }
+	    else{
+	        System.out.println(counter+"even");
+	    }
+	        
+	    }
+	   
+	    
+	    
+	}
+}
